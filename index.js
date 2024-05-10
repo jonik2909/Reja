a7e29558887f17df422dc70d3e1585a762808b99
jonik2909 <boronovjonibek@gmail.com>
2024-05-11 00:11:30 +0900

    fix: modify index js

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const cluster = require("cluster");
 
 if (cluster.isMaster) {
   console.log(`MASTER thread process id => ${process.pid}`);
-  instanceCPUs.map(() => {
-    cluster.fork();
-  });
 } else {
   console.log(`WORKER thread process id => ${process.pid}`);
 }
