1af678a36bbd52e5a63cbc225bdca02fde173e41
jonik2909 <boronovjonibek@gmail.com>
2024-05-11 00:18:45 +0900

    feat: Modullar - File Module

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const instanceCPUs = require("os").cpus();
 const cluster = require("cluster");
 
 if (cluster.isMaster) {
-  console.log(`MASTER thread process id => ${process.pid}`);
+  console.log(`MASTER thread process id `);
 } else {
   console.log(`WORKER thread process id => ${process.pid}`);
 }
